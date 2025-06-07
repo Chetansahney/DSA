df222efcdcbf237936ecb6d51d5dc579bdd7dc9f
Chetan Sahney <84283700+Chetansahney@users.noreply.github.com>
2025-06-07 11:18:48 +0530

    Time: 2645 ms (5.01%) | Memory: 40.2 MB (83.55%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPerfectSquare(int num) 
+    {
+        if(num==1||num==4)
+        return true;
+       
+        for(int i=0;i<=num/3;i++)
+        {
+            if(i*i==num)
+            return true;
+        }
+        return false;
+    }
+}

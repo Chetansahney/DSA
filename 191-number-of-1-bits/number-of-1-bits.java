5b976068bee7c8c88e7b23843337c686336d36d9
Chetan Sahney <84283700+Chetansahney@users.noreply.github.com>
2025-10-23 02:22:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (74.32%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n &= (n - 1);  // remove the lowest set bit
+            count++;
+        }
+        return count;
+    }
+}
+//Brian Kernighan’s Algorithm

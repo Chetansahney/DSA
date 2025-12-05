0c6265dabd50b5ddd0eb3ade0ff2c03930ad800f
Chetan Sahney <84283700+Chetansahney@users.noreply.github.com>
2025-12-06 00:43:15 +0530

    Time: 3 ms (97.72%) | Memory: 84.5 MB (40.02%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] result = new int[n];
+
+        
+        for(int[] booking : bookings) {
+            int first = booking[0];
+            int last = booking[1];
+            int seats = booking[2];
+
+            result[first - 1] += seats;    
+
+            if(last < n) {
+                result[last] -= seats;     
+            }
+        }
+        
+        for(int i = 1; i < n; i++) {
+            result[i] += result[i - 1];
+        }
+
+        return result;
+    }
+}

7b443fdbb7379b1bda5d8ab1c017e38be3d62cf5
Chetan Sahney <84283700+Chetansahney@users.noreply.github.com>
2025-12-22 17:34:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (27.01%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingDistance(int x, int y) 
+    {
+      int count=0;
+      int d=x^y;
+      while(d!=0)
+      {
+        d&=d-1;
+        count++;
+      }  
+      return count;
+    }
+}
